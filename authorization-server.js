94faa629a8e49421dcc2ab79cc911a8aa7295c31
weirdyang <wy4786@gmail.com>
2021-03-15 15:52:09 +0800

    step 10 complete

--- authorization-server.js
+++ authorization-server.js
@@ -1,64 +1,110 @@
-const fs = require("fs")
-const express = require("express")
-const bodyParser = require("body-parser")
-const jwt = require("jsonwebtoken")
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 const {
-	randomString,
-	containsAll,
-	decodeAuthCredentials,
-	timeout,
-} = require("./utils")
+  randomString,
+  containsAll,
+  decodeAuthCredentials,
+  timeout,
+} = require('./utils');
 
 const config = {
-	port: 9001,
-	privateKey: fs.readFileSync("assets/private_key.pem"),
+  port: 9001,
+  privateKey: fs.readFileSync('assets/private_key.pem'),
 
-	clientId: "my-client",
-	clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-	redirectUri: "http://localhost:9000/callback",
+  clientId: 'my-client',
+  clientSecret: 'zETqHgl0d7ThysUqPnaFuLOmG1E=',
+  redirectUri: 'http://localhost:9000/callback',
 
-	authorizationEndpoint: "http://localhost:9001/authorize",
-}
+  authorizationEndpoint: 'http://localhost:9001/authorize',
+};
 
 const clients = {
-	"my-client": {
-		name: "Sample Client",
-		clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-		scopes: ["permission:name", "permission:date_of_birth"],
-	},
-	"test-client": {
-		name: "Test Client",
-		clientSecret: "TestSecret",
-		scopes: ["permission:name"],
-	},
-}
+  'my-client': {
+    name: 'Sample Client',
+    clientSecret: 'zETqHgl0d7ThysUqPnaFuLOmG1E=',
+    scopes: ['permission:name', 'permission:date_of_birth'],
+  },
+  'test-client': {
+    name: 'Test Client',
+    clientSecret: 'TestSecret',
+    scopes: ['permission:name'],
+  },
+};
 
 const users = {
-	user1: "password1",
-	john: "appleseed",
-}
+  user1: 'password1',
+  john: 'appleseed',
+};
 
-const requests = {}
-const authorizationCodes = {}
+const requests = {};
+const authorizationCodes = {};
 
-let state = ""
+const state = '';
 
-const app = express()
-app.set("view engine", "ejs")
-app.set("views", "assets/authorization-server")
-app.use(timeout)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'assets/authorization-server');
+app.use(timeout);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 Your code here
 */
+app.get('/authorize', (req, res) => {
+  const clientId = req.query.client_id;
+  if (!(clientId in clients)) {
+    return res.status(401).send('no such client');
+  }
+  const { scope } = req.query;
+  if (!scope || !containsAll(clients[clientId].scopes, scope.split(' '))) {
+    return res.status(401).send('invalid scope');
+  }
+  const key = randomString();
+  requests[key] = req.query;
+  return res.render('login', {
+    client: clients[clientId],
+    scope,
+    requestId: key,
+  });
+});
 
-const server = app.listen(config.port, "localhost", function () {
-	var host = server.address().address
-	var port = server.address().port
-})
+app.post('/approve', (req, res) => {
+  const { userName, password, requestId } = req.body;
+  if (!users[userName] || !password || !requestId) {
+    console.log('1');
+    return res.status(401).send('invalid body');
+  }
+  if (!users[userName] && users[userName] !== password) {
+    console.log('2');
+    return res.status(401).send('invalid login details');
+  }
+  if (!requests[requestId]) {
+    console.log('3');
+    return res.status(401).send('invalid request id');
+  }
+  const clientReq = requests[requestId];
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
+  delete requests[requestId];
+  const key = randomString();
+  authorizationCodes[key] = { clientReq, userName };
+  // eslint-disable-next-line camelcase
+  const { redirect_uri, state } = clientReq;
+  const redirectUrl = new URL(redirect_uri);
+  redirectUrl.searchParams.append('code', key);
+  redirectUrl.searchParams.append('state', state);
+  
+  return res.redirect(redirectUrl.href);
+});
+const server = app.listen(config.port, 'localhost', () => {
+  const host = server.address().address;
+  const { port } = server.address();
+});
 
 // for testing purposes
 
-module.exports = { app, requests, authorizationCodes, server }
+module.exports = {
+  app, requests, authorizationCodes, server,
+};
--- test/module1/02-verify-client-id.js
+++ test/module1/02-verify-client-id.js
@@ -1,26 +1,24 @@
-const assert = require("assert")
-const request = require("supertest")
+const assert = require('assert');
+const request = require('supertest');
 
-const { app } = require("../../authorization-server")
+const { app } = require('../../authorization-server');
 
-it("returns a 200 for a valid client ID @authorization-server-verify-client-id", () => {
-	return request(app)
-		.get("/authorize?client_id=my-client&scope=permission:name")
-		.then((res) => {
-			assert.notEqual(res.status, 404, "The `/authorize` route doesn't exist")
-			assert.notEqual(
-				res.status,
-				401,
-				"The `/authorize` route should not return a 401 status if the client ID is valid"
-			)
+it('returns a 200 for a valid client ID @authorization-server-verify-client-id', () => request(app)
+  .get('/authorize?client_id=my-client&scope=permission:name')
+  .then((res) => {
+    assert.notEqual(res.status, 404, "The `/authorize` route doesn't exist");
+    assert.notEqual(
+      res.status,
+      401,
+      'The `/authorize` route should not return a 401 status if the client ID is valid',
+    );
 
-			return request(app).get("/authorize?client_id=fake-client")
-		})
-		.then((res) => {
-			assert.equal(
-				res.status,
-				401,
-				"The `/authorize` route should return a 401 status if the client ID is invalid"
-			)
-		})
-})
+    return request(app).get('/authorize?client_id=fake-client');
+  })
+  .then((res) => {
+    assert.equal(
+      res.status,
+      401,
+      'The `/authorize` route should return a 401 status if the client ID is invalid',
+    );
+  }));
